031e619a4dcbc782caee168404dbd24f222ef22b
Millie <Minnie-Million@users.noreply.github.com>
2020-02-25 13:10:51 -0800

    Update Bullet.java

--- Bullet.java
+++ Bullet.java
@@ -10,7 +10,7 @@ public class Bullet extends SmoothMover
 {
     /** The damage this bullet will deal */
     private static final int damage = 16;
-    
+    private static final int pointsToAdd = 3;
     /** A bullet looses one life each act, and will disappear when life = 0 */
     private int life = 30;
     
@@ -55,6 +55,7 @@ public class Bullet extends SmoothMover
         Asteroid asteroid = (Asteroid) getOneIntersectingObject(Asteroid.class);
         if (asteroid != null)
         {
+            ((Space) getWorld()).updateScore(pointsToAdd);
             getWorld().removeObject(this);
             asteroid.hit(damage);
         }
